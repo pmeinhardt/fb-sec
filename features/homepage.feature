9c8501dd15875a9caccc47ece5f1b006f7a12f1b
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2013-02-07 18:51:55 +0100

    Initial working cucumber feature

--- features/homepage.feature
+++ features/homepage.feature
@@ -3,4 +3,6 @@ Feature: Homepage
   @javascript
   Scenario: Generate a bookmark
     Given I go to the homepage
-     Then I see "fb-sec"
+      And I wait for the bookmark to be generated
+     Then I get a public key for copying
+      And I get a bookmark link
--- features/steps/homepage_steps.rb
+++ features/steps/homepage_steps.rb
@@ -1,3 +1,25 @@
 Given /^I go to the homepage$/ do
   visit "/"
 end
+
+Given /^I wait for the bookmark to be generated$/ do
+  started = Time.now
+
+  begin
+    find_field("pubkey").value.should_not be_empty
+  rescue => e
+    raise e if (Time.now - started) > 10.0
+    sleep 0.1
+    retry
+  end
+end
+
+Then /^I get a public key for copying$/ do
+  value = find_field("pubkey").value
+  value.should match(/\[\[fbskey\:[a-z0-9]+\|10001\]\]/)
+end
+
+Then /^I get a bookmark link$/ do
+  anchor = find("#bookmarklet")
+  anchor["href"].should match(/\Ajavascript:(.*)\z/)
+end
